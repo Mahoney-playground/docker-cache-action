605c4cd9b8a232c710f917979ddf4d410ff03007
Robert Elliot <rob@lidalia.org.uk>
2020-03-01 22:25:23 +0000

    One more time on the caching

--- backup.sh
+++ backup.sh
@@ -6,12 +6,12 @@ function main {
   local me
   me=$(whoami)
   local cache_dir=~/docker_cache
-  local cache_tar="$cache_dir/cache.tgz"
+  local cache_tar="$cache_dir/cache.tar"
   mkdir -p "$cache_dir"
   rm -f "$cache_tar"
 
   sudo service docker stop
-  sudo /bin/tar -cz -f "$cache_tar" -C /var/lib/docker .
+  sudo /bin/tar -c -f "$cache_tar" -C /var/lib/docker .
   sudo chown "$me:$me" "$cache_tar"
   sudo service docker start
 }
--- restore.sh
+++ restore.sh
@@ -3,13 +3,13 @@
 set -exuo pipefail
 
 function main() {
-  local cache_tar=~/docker_cache/cache.tgz
+  local cache_tar=~/docker_cache/cache.tar
 
   if [[ -f "$cache_tar" ]]; then
     sudo service docker stop
     sudo rm -rf /var/lib/docker
     sudo mkdir -p /var/lib/docker
-    sudo tar -zxf "$cache_tar" -C /var/lib/docker
+    sudo tar -xf "$cache_tar" -C /var/lib/docker
     sudo service docker start
   else
     docker system prune -a -f --volumes
