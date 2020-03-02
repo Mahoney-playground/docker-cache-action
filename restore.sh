2e040fd600687ead377f5e78a8be15b5af383174
Robert Elliot <a-relliot@expedia.com>
2020-03-02 11:37:51 +0000

    rm was quicker *shrug*

--- restore.sh
+++ restore.sh
@@ -8,9 +8,8 @@ function main() {
   if [[ -f "$cache_tar" ]]; then
     ls -lh "$cache_tar"
     time sudo service docker stop
-    local empty_dir
-    empty_dir=$(mktemp -d)
-    time sudo rsync -a --delete "$empty_dir/" /var/lib/docker/
+    time sudo rm -rf /var/lib/docker
+    sudo mkdir -p /var/lib/docker
     time sudo tar -xf "$cache_tar" -C /var/lib/docker
     time sudo service docker start
   else
