70e45491d0c2b207e0ba11739e8cf3d4af2d3a13
Robert Elliot <a-relliot@expedia.com>
2020-03-02 11:32:20 +0000

    Safer empty dir 2

--- restore.sh
+++ restore.sh
@@ -9,7 +9,7 @@ function main() {
     ls -lh "$cache_tar"
     time sudo service docker stop
     local empty_dir
-    empty_dir=$(mktemp -d empty_dir)
+    empty_dir=$(mktemp -d)
     time sudo rsync -a --delete "$empty_dir" /var/lib/docker/
     time sudo tar -xf "$cache_tar" -C /var/lib/docker
     time sudo service docker start
