ac547a51a61d552a10409a5bcc8c83099e68c5ad
Robert Elliot <a-relliot@expedia.com>
2020-03-02 11:04:21 +0000

    Get sizing before restore as well as after backup

--- restore.sh
+++ restore.sh
@@ -6,6 +6,7 @@ function main() {
   local cache_tar=~/docker_cache/cache.tar
 
   if [[ -f "$cache_tar" ]]; then
+    ls -lh "$cache_tar"
     time sudo service docker stop
     time sudo rm -rf /var/lib/docker/*
     time sudo tar -xf "$cache_tar" -C /var/lib/docker
