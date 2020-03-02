202f4ccab1180781389e3f87c5cf980897908a40
Robert Elliot <a-relliot@expedia.com>
2020-03-02 11:13:54 +0000

    Revert back to delete and recreate /var/lib/docker
    Otherwise we backup unwanted dirs

--- restore.sh
+++ restore.sh
@@ -9,7 +9,8 @@ function main() {
   if [[ -f "$cache_tar" ]]; then
     ls -lh "$cache_tar"
     time sudo service docker stop
-    time sudo rm -rf /var/lib/docker/*
+    time sudo rm -rf /var/lib/docker
+    sudo mkdir -p /var/lib/docker
     time sudo tar -xf "$cache_tar" -C /var/lib/docker
     time sudo service docker start
   else
