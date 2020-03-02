a8cf32afd1a436f0f76582ff3f37f16632631a8c
Robert Elliot <a-relliot@expedia.com>
2020-03-02 11:27:32 +0000

    Fix it...

--- restore.sh
+++ restore.sh
@@ -9,7 +9,7 @@ function main() {
     ls -lh "$cache_tar"
     time sudo service docker stop
     mkdir empty_dir
-    rsync -a --delete empty_dir/ /var/lib/docker/
+    time sudo rsync -a --delete empty_dir/ /var/lib/docker/
     time sudo tar -xf "$cache_tar" -C /var/lib/docker
     time sudo service docker start
   else
